ead437fcb4cf9922233f3abf552896ae02f9bf32
Yassine-Eljaouhary36 <yassineeljaouhay@gmail.com>
2022-03-20 13:23:57 +0100

    Add: Option to specifies that processe are forcibly terminated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,14 +15,16 @@ class HomeController extends Controller
     
     public function runCommand(Request $request){
         $validated = $request->validate([
-            'command' => 'required|max:255',
+            'command' => 'required',
         ]);
         $command = $validated["command"];
+        $handle = popen("start /B ". $command, "r");
         $commandCheck = Process::where("command","$command")->count();
         if($commandCheck>0){
             return redirect()->route('Home')->with('status', 'Command Already Existed');
         }else{
             $cmd = 'wmic process call create "'.$command.'" | find "ProcessId"';
+            // $cmd = $command;
             $handle = popen("start /B ". $cmd, "r");
             $read = fread($handle, 200); 
             $pid=substr($read,strpos($read,'=')+1);
@@ -67,10 +69,11 @@ class HomeController extends Controller
 
     // To Kill The Proccess After Check her Status
     public function KillPross(Request $request){
+            
         $pid=$request->pid;
         $result = shell_exec('tasklist /FI "PID eq '.$pid.'"' );
         if (count(preg_split("/\n/", $result))>2) {
-            shell_exec('taskkill /PID '.$pid );
+            shell_exec('taskkill /PID '.$pid.' /F' );
             return redirect()->route('Home')->with('statusCheckNotGood', 'Command Stoped successfuly !');
         }else{
             return redirect()->route('Home');
